886aa07147f78ea85a0ce5becebe0c3880b33b95
Hugo Valente <hugomika68@gmail.com>
2020-11-17 16:27:06 +0100

    Initial commit
    Current status showing index with all posts.
    Next redirecting on click to full details of post

---
+++ blog/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.urls import reverse
+
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    summary = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255, unique= True)
+    published = models.BooleanField(default= True)
+    content = models.TextField(default="")
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+
+        def __unicode__(self):
+            return u'%s' % self.title
+
+    def get_absolute_url(self):
+        return reverse('blogs.views.post', args=[self.slug])
